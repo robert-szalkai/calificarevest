227d06fadac4469690127f0b73aa0b9f04cbd329
szroby29 <robert.szalkai.work@gmail.com>
2021-04-24 17:11:27 +0300

    contact function

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from '../components/Form'
+
+import '../global.css'
+
+const Index = () => {
+    return (
+        <>
+            <h1>Send a message</h1>
+            <Form />
+        </>
+    )
+}
+
+export default Index
