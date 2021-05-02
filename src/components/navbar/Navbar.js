a298ba2abf3a8ba6f59c76475253fd43891e4444
szroby29 <robert.szalkai.work@gmail.com>
2021-05-02 20:08:26 +0300

    fixed build error

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -7,8 +6,8 @@ const Navbar = () => {
             <nav className="center-layout flex flex-between">
                 <a className="h1" href="/">Cursuri Calificare Vest</a>
                 <div>
-                    <Link to="/">Acasa</Link>
-                    <Link to="/cursuri">Cursuri</Link>
+                    <a href="/">Acasa</a>
+                    <a href="/cursuri">Cursuri</a>
                 </div>
             </nav>
         </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,21 @@ import React from 'react'
 import Form from '../components/Form'
 import Navbar from '../components/navbar/Navbar'
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import '../global.css'
 import '../styles/layout.css'
 import '../styles/typography.css'
 import Courses from '../components/courses/Courses';
-import { Home } from '../components/home/Home';
+import Home from '../components/home/Home'
 
 const Index = () => {
     return (
         <div className="body-container">
-            <Router>
-                <Navbar />
-                <h1>Send a message</h1>
-                <Form />
-
-                <Switch>
-                    <Route path="/cursuri">
-                        <Courses />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
+            <Navbar />
+            <h1>Send a message</h1>
+            <Form />
+            <Courses />
+            <Home />
         </div>
     )
 }
