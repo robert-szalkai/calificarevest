cd299a17679c6c1873a54b2d9e65a0d6cc9957b3
szroby29 <robert.szalkai.work@gmail.com>
2021-05-05 21:04:33 +0300

    hero component

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import * as styles from './Hero.module.css';
+
+const Hero = () => {
+    return (
+        <div className={styles.container}>
+            <div className="text-center md:text-left grid justify-center">
+                <h2 className="mb-6">
+                    Cursuri <br/> 
+                    de calificare
+                </h2>
+                <p className="mb-3">
+                    Construieste-ti cariera dorita, pas cu pas,<br/> 
+                    cu ajutorul servicilor noastre de cea mai <br/> 
+                    inalta calitate
+                </p>
+                <p className="uppercase mb-5"> Toate pe teritoriul Romaniei</p>
+                <button className="btn btn-green btn-big">Vezi cursurile</button>
+            </div>
+            <img src={'hero.svg'} alt="teacher"/>
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Form from '../components/Form'
+import Form from '../components/form'
+import Hero from '../components/hero/hero'
 import Layout from '../components/layout/Layout'
 
 
@@ -10,6 +11,9 @@ import '../styles/typography.css'
 const Index = () => {
     return (
         <Layout>
+            <section className="center-layout mt-16 md:mt-32 mb-16 md:mb-32">
+                <Hero />
+            </section>
             <section className="center-layout">
                 <Form />
             </section>
