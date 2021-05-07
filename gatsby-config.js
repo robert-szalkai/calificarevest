6f831e81c656537e273ef991082354497f375768
szroby29 <robert.szalkai.work@gmail.com>
2021-05-07 09:45:55 +0300

    added manifest json favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
         title: "Cursuri de formare profesionala",
         description: "Invata meserie la cursuri de calificare vest"
     },
-    plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-react-helmet']
+    plugins: [
+        'gatsby-plugin-postcss', 
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Centru de servicii',
+                icon: 'static/favicon.png'
+            }
+        }
+    ]
 }
