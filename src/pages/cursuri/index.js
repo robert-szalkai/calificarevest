56f341e5df0fe1972187f13be43a297a164f033a
szroby29 <robert.szalkai.work@gmail.com>
2021-10-07 17:19:37 +0300

    fix 2

--- src/pages/cursuri/index.js
+++ src/pages/cursuri/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import Cursuri from '../../components/cursuri/Cursuri'
+import useAirtable from '../../hooks/useAirtable';
 
-const index = () => {
+const Index = () => {
+    let cursuri = useAirtable();
     return (
         <Layout>
             <section className="full-width-layout main-layout min-h-screen bg-blue-100">
                 <div className="center-layout mt-10 md:mt-20 xl:mt-32 mb-24 md:mb-32 overflow-hidden">
-                    <Cursuri />
+                    <Cursuri cursuri={cursuri} />
                 </div>
             </section>
         </Layout>
     )
 }
 
-export default index
+export default Index
